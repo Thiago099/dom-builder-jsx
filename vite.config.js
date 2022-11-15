b693b252ca446da17e68c74ae50bfee0d01cf3a3
Thiago Kaique <thiago.kaique@stayfilm.com.br>
2022-11-15 19:53:26 -0300

    [feature]

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,5 @@ import  jsxDomBuilderVitePlugin  from "jsx-dom-builder/vite-plugin"
 // custom jsx pragma
 export default defineConfig({
   plugins:[jsxDomBuilderVitePlugin()],
+  base: '/jsx-dom-builder-vite-example /',     
 })
