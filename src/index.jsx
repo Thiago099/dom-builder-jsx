9f21047a5d491e7486c313399319912ead2849e0
Thiago Kaique <thiago.kaique@stayfilm.com.br>
2022-11-15 20:49:35 -0300

    [feature]

--- src/index.jsx
+++ src/index.jsx
@@ -3,8 +3,13 @@ import Counter from "./components/counter.jsx"
 import Title from "./components/title.jsx"
 import RefExample from "./components/ref-example.jsx"
 
-<div parent={document.body} class="container">
+
+
+const app = 
+<div class="container">
     <Title text="vite + jsx-dom-builder"/>
     <Counter />
     <RefExample />
 </div>
+
+app.parent(document.body)
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite"
 import  jsxDomBuilderVitePlugin  from "jsx-dom-builder/vite-plugin"
 // custom jsx pragma
+
 export default defineConfig({
   plugins:[jsxDomBuilderVitePlugin()],
-  base: '/jsx-dom-builder-vite-example /',     
+  base: '/jsx-dom-builder-vite-example/',   
 })
