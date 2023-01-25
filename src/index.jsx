e00692dbbfebba379e20f2c5587454e635b7a3c1
Thiago <thiago.kaique@stayfilm.com.br>
2023-01-25 10:00:48 -0300

    refactor

--- src/index.jsx
+++ src/index.jsx
@@ -40,10 +40,11 @@ function card({data})
 
     header.mounted(() => {
         var size = 30;
+        header.style.fontSize = size + "px"
         while(header.offsetWidth > 400)
         {
-            header.style.fontSize = size + "px"
             size -= 1
+            header.style.fontSize = size + "px"
         }
     })
 
@@ -53,11 +54,15 @@ function card({data})
         {
             if(name instanceof entity)
             {
-                return name.data.Name
+                if(name.data.Propery.Any)
+                {
+                    return <div style="color:blue">{name.data.Name}</div>
+                }
+                return <div>{name.data.Name}</div>
             }
             if(Array.isArray(name))
             {
-                return name.map(x=><div>{parse_prop(x)}</div>)
+                return name.map(x=>parse_prop(x))
             }
             return name
         }
@@ -74,32 +79,52 @@ function card({data})
     return result
 }
 
+function isAny(inpersonation, entity)
+{
+    for(const [key, value] of Object.entries(inpersonation.data.Propery))
+    {
+        if(key == "Any")
+        {
+            if(entity.data.Propery["Generic"] != value)
+            {
+                return false
+            }
+        }
+        else
+        if(entity.data.Propery[key] != value)
+        {
+            return false
+        }
+    }
+    return true
+}
+
 const main = 
 <div class="container">
 </div>
 
 const hot = entity("Hot",{})
-const objects = entity("Object",{})
+const object = entity("Object",{})
 const touch = entity("Touch",{})
 const burn = entity("Burn",{})
 const causes = entity("Causes",{})
 const bare_hands = entity("Bare hands", {})
 
 const pan_handle = entity("Pan Handle", {})
-const pan_body = entity("Pan Body", { Temperature: hot })
-const pan = entity("Pan", {Parts:[pan_handle, pan_body]})
+const pan_body = entity("Pan Body", { Generic:object,Temperature: hot })
+// const pan = entity("Pan", {Parts:[pan_handle, pan_body]})
 
 const touch_pan_body = entity("Touch pan body", {  Generic:touch ,Actor: bare_hands ,Subject: pan_body})
 
-const _something_hot = entity("Something hot", { Generic: objects,Temperature: hot })
+const _something_hot = entity("<Something hot>", { Any: object,Temperature: hot })
 
 const _touch = entity("Touch something hot", {  Generic: touch ,Actor: bare_hands ,Subject: _something_hot})
 
 const _burn = entity("Burn your hands", { Generic:burn,Actor: _something_hot ,Subject: bare_hands})
 
-const _causes = entity("Touch something hot cases you to burn your hands", { Generic:touch, Cause: _touch, Effect: _burn })
+const _causes = entity("Touch something hot cases you to burn your hands", { Generic:causes, Cause: _touch, Effect: _burn })
 
-
+console.log(isAny(_something_hot,pan_body))
 
 
 main.parent(document.body)
