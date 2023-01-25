9b9356c4a6f0e3b01f38714940dc69859dcfbc88
Thiago <thiago.kaique@stayfilm.com.br>
2023-01-24 22:06:42 -0300

    progress

--- src/index.jsx
+++ src/index.jsx
@@ -7,6 +7,11 @@ function uuidv4() {
   }
 
 function entity(name, properties){
+    if(!this) 
+    {
+        return new entity(name, properties);
+    }
+
     this.data = {
         Name: name,
         Propery: properties,
@@ -18,15 +23,29 @@ function entity(name, properties){
 function card({data})
 {
     const body = ref()
+    const header = ref()
+
+
     var result =
     <div class="card">
         <div class="card-header" style="color:green">
-            {data.Name}
+            <span ref={header}>
+                {data.Name}
+            </span>
             {/* <span class="id">{data.Id}</span> */}
-        </div>
+        </div >
         <div class="card-body" ref={body} if={data.Propery && Object.entries(data.Propery).length > 0}>
         </div>
     </div>
+
+    header.mounted(() => {
+        var size = 30;
+        while(header.offsetWidth > 400)
+        {
+            header.style.fontSize = size + "px"
+            size -= 1
+        }
+    })
 
     for(const Propery in data.Propery)
     {
@@ -59,21 +78,26 @@ const main =
 <div class="container">
 </div>
 
-const hot = new entity("Hot",{})
+const hot = entity("Hot",{})
+const objects = entity("Object",{})
+const touch = entity("Touch",{})
+const burn = entity("Burn",{})
+const causes = entity("Causes",{})
+const bare_hands = entity("Bare hands", {})
 
-const pan_handle = new entity("Pan Handle", {})
-const pan_body = new entity("Pan Body", { Temperature: hot })
-const pan = new entity("Pan", {Parts:[pan_handle, pan_body]})
+const pan_handle = entity("Pan Handle", {})
+const pan_body = entity("Pan Body", { Temperature: hot })
+const pan = entity("Pan", {Parts:[pan_handle, pan_body]})
 
-const bare_hands = new entity("Bare hands", {})
+const touch_pan_body = entity("Touch pan body", {  Generic:touch ,Actor: bare_hands ,Subject: pan_body})
 
-const _something_hot = new entity("<Something Hot>", { Temperature: hot })
+const _something_hot = entity("Something hot", { Generic: objects,Temperature: hot })
 
-const touch = new entity("Touch", { Subject: _something_hot, Actor: bare_hands })
+const _touch = entity("Touch something hot", {  Generic: touch ,Actor: bare_hands ,Subject: _something_hot})
 
-const burn = new entity("Burn", { Subject: bare_hands, Actor: _something_hot })
+const _burn = entity("Burn your hands", { Generic:burn,Actor: _something_hot ,Subject: bare_hands})
 
-const causes = new entity("Causes", { Subject: touch, Actor: burn })
+const _causes = entity("Touch something hot cases you to burn your hands", { Generic:touch, Cause: _touch, Effect: _burn })
 
 
 
